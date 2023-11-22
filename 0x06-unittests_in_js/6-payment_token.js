3f8009710eba63f6f31879bdc2a97256d0ef2155
Sommyj1 <113688978+Sommyj1@users.noreply.github.com>
2023-11-22 13:21:30 -0800

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,7 +0,0 @@
-const getPaymentTokenFromAPI = (success) => {
-  if (success) {
-    return new Promise(resolve => resolve({data: 'Successful response from the API' }));
-  }
-}
-
-module.exports = getPaymentTokenFromAPI;
